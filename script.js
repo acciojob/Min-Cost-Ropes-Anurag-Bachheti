2d3a5284e4a5431c604d5836cb05dd32b07da3ae
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-03-16 12:35:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr){
+if(arr.length < 2) return 0;
+	
+	let newArr = arr.sort((a, b)=> a - b);
+	let count = 0;
+
+	while(newArr.length > 1){
+		let first = newArr.shift();
+		let second = newArr.shift();
+		let sum = first + second;
+		cost += sum;
+
+		let index = newArr.findIndex(el => el > sum);
+		if(index === -1){
+			newArr.push(sum);
+		}else{
+			newArr.splice(index, 0, sum);
+		}
+	}
+	return cost;
 }
 
 module.exports=mincost;
